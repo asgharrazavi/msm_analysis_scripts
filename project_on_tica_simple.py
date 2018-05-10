0097c1eb79289de303f30f04217c3316dcc9cf36
Asghar Razavi <asghar.razavi@gmail.com>
2018-05-10 07:55:35 -0400

    improved project_on_tica_simple.py

--- project_on_tica_simple.py
+++ project_on_tica_simple.py
@@ -12,31 +12,32 @@ rcParams.update({'figure.autolayout': True})
 rcParams['axes.linewidth'] = 3
 rcParams.update({'font.size': 20})
 
-parms = np.loadtxt('parms.txt',dtype=str)
-
-def project(start,stop,stride,cc_path,parms):
-    n_parms = len(parms)
+def project():
     dataset = []
     for i in range(50):
-    	ref1 = io.loadh('%s/traj%d_cc.h5' %(cc_path,i), name = 'ARG60-CZ--ARG60-CZ')[start:stop:stride]
+	ref1 = io.loadh('%s/traj%d_cc.h5' %(traj_path,i), name=parms[0])
     	d = np.zeros((len(ref1),n_parms))
     	print "working on:", i
-    	for p in range(n_parms):
-		data = io.loadh('%s/traj%d_cc.h5' %(cc_path,i), name=parms[p])
-	        d[:,p][0:d.shape[0]] = data[start:start+d.shape[0]]
+    	for p in range(len(parms)):
+	    data = io.loadh('%s/traj%d_cc.h5' %(traj_path,i), name=parms[p])
+	    d[:,p] = data
         proj = np.dot(d,tica['components'].T)
 	io.saveh('on_tica_l16ns_%d.h5' %(i), proj)
         dataset.append(proj)
     return dataset
 
-tica = io.loadh('tica_l16ns.h5')
-cc_path = '/Users/asr2031/Desktop/transfer/dDAT_WT_ensemble_stampede/cc_dists/1to1252_skip20_cc'
 
-t6 = project(0,-1,1,cc_path,parms)
+parms_file = "params.txt"
+traj_path = "../trajs/"
+
+parms = np.loadtxt('%s' %parms_file,dtype=str)
+tica = io.loadh('tica_l16ns.h5')
+
+proj_data = project()
 
 ev0, ev1, ev2 = [], [], []
 for i in range(50):
-    ev0.extend(t6[i][:,0]); ev1.extend(t6[i][:,1]); ev2.extend(t6[i][:,2])
+    ev0.extend(proj_data[i][:,0]); ev1.extend(proj_data[i][:,1]); ev2.extend(t6[i][:,2])
 ev0, ev1, ev2 = np.array(ev0), np.array(ev1), np.array(ev2)
 io.saveh('ev0.h5',ev0)
 io.saveh('ev1.h5',ev1)
