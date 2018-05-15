346c79a054cdceef80d88e33b9434281bd3f9fcc
Asghar Razavi <asghar.razavi@gmail.com>
2018-05-15 07:42:39 -0400

    added plot_tica_evs.py

--- plot_tica_evs.py
+++ plot_tica_evs.py
@@ -23,16 +23,17 @@ plt.plot(evs[:,1]/float(np.sum(abs(evs[:,1]))),'-bo',linewidth=2)
 plt.plot(evs[:,2]/float(np.sum(abs(evs[:,2]))),'-go',linewidth=2)
 plt.plot([-0.5,len(parms)],[0,0],'k')
 
+# labeling axes and fancy legend
 plt.legend(('tIC 1','tIC 2','tIC 3'),fontsize=20,ncol=3,handletextpad=0.2,shadow=True,fancybox=True,columnspacing=1,labelspacing=0.1,loc='upper center')
 plt.ylabel('Normalized Eigenvector')
 plt.xlabel('tICA Parameters')
 
 plt.xlim([-0.5,len(parms)+1])
 plt.xticks(range(len(parms)),parms,rotation='vertical',fontsize=12)
-plt.savefig('evs.png',dpi=100)
+plt.savefig('tICA_evs.png',dpi=100)
 
 # plot eigenvalues as well 
 plt.figure(figsize=(10,10))
 plt.plot(ti['vals'],'o-')
-plt.savefig('vals.png')
+plt.savefig('tICA_vals.png')
 
