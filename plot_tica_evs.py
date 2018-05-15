a86e47736c918ec5a94697a47c317a33ff917324
Asghar Razavi <asghar.razavi@gmail.com>
2018-05-15 07:38:35 -0400

    plot_tica_evs.py

---
+++ plot_tica_evs.py
@@ -0,0 +1,33 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import mdtraj.io as io
+from matplotlib import rcParams
+rcParams.update({'figure.autolayout': True})
+rcParams['axes.linewidth'] = 2
+rcParams.update({'font.size': 20})
+
+# load parameters for labeling
+parms = np.loadtxt('parms.txt',dtype=str)
+print len(parms)
+
+# load tICA and extract tICA eigenvectors
+ti = io.loadh('tica_l16ns.h5')
+evs = ti['vecs']
+
+plt.figure(figsize=(30,7))
+plt.plot(evs[:,0]/float(np.sum(abs(evs[:,0]))),'-ro',linewidth=2)
+plt.plot(evs[:,1]/float(np.sum(abs(evs[:,1]))),'-bo',linewidth=2)
+plt.plot(evs[:,2]/float(np.sum(abs(evs[:,2]))),'-go',linewidth=2)
+plt.plot([-0.5,len(parms)],[0,0],'k')
+plt.legend(('tIC 1','tIC 2','tIC 3'),fontsize=20,ncol=3,handletextpad=0.2,shadow=True,fancybox=True,columnspacing=1,labelspacing=0.1,loc='upper center')
+plt.ylabel('Normalized Eigenvector')
+plt.xlabel('tICA Parameters')
+plt.xlim([-0.5,len(parms)+1])
+plt.xticks(range(len(parms)),parms,rotation='vertical',fontsize=12)
+plt.savefig('evs.png',dpi=100)
+#plt.show()
+
+plt.figure(figsize=(10,10))
+plt.plot(ti['vals'],'o-')
+plt.savefig('vals.png')
+
