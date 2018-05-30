ea58b7c4d51977d1c06e8295429d4e7e2ea520d3
Asghar Razavi <asghar.razavi@gmail.com>
2018-05-30 07:38:30 -0400

    0_get_assignments.py

--- Opsin_Structure/assigns/0_get_assignments.py
+++ Opsin_Structure/assigns/0_get_assignments.py
@@ -9,7 +9,7 @@ cluster = KMeans(n_clusters=100,n_jobs=-1,verbose=0, max_iter=100, tol=0.0001,)
 dataset = []
 for i in range(4):
     a = io.loadh('../on_tica_l20_s1_%d.h5' %i)['arr_0']
-    a = a[:,0:3]
+    a = a[:,0:3]		# using first 3 tICs
     dataset.append(a)
     print a.shape
 for i in range(20):
@@ -29,8 +29,8 @@ for i in range(20):
     print a.shape
 
 cluster.fit(dataset)
+lens = [len(i) for i in dataset]
 print np.array(cluster.labels_).shape
 np.savetxt('gens.txt',np.array(cluster.cluster_centers_))
 for i in range(64):
     np.savetxt('assigns_%d.txt' %i,np.array(cluster.labels_[i]),fmt='%d')
-#np.savetxt('gens_id2.txt',np.array(cluster.cluster_ids_))
