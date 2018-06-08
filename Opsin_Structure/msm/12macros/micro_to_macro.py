45df988fd2f8d051f6b2397d559e69b0d64ddfef
Asghar Razavi <asghar.razavi@gmail.com>
2018-06-08 08:09:40 -0400

    micro_to_macro.py

---
+++ Opsin_Structure/msm/12macros/micro_to_macro.py
@@ -0,0 +1,21 @@
+import os,sys
+import mdtraj.io as io
+import numpy as np
+
+map15 = np.loadtxt('map12_pccaplus.dat')
+
+def mapp(micro_assigns,macro_map):
+    n_macros = int(np.max(macro_map)) + 1
+    macro_assigns = np.zeros(micro_assigns.shape)
+    for i in range(n_macros):
+	ind_map = np.where(macro_map == i)[0]
+	for m in ind_map:
+		ind = (micro_assigns == m)
+		macro_assigns[ind] = i
+    return macro_assigns 
+
+for i in range(64):
+    micro_assigns = np.loadtxt('../../assigns/assigns_%d.txt' %(i),dtype=int)
+    macro_assigns = mapp(micro_assigns,map15)
+    print "i, micro_assigns.shape:", i, micro_assigns.shape
+    np.savetxt('macro12_assigns_%d.txt' %i, macro_assigns, fmt='%d')
