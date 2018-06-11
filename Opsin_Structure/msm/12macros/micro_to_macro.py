6fada7b3f8af6240f17838acd07c3c397c3eb0cd
Asghar Razavi <asghar.razavi@gmail.com>
2018-06-11 07:31:14 -0400

    micro_to_macro.py

--- Opsin_Structure/msm/12macros/micro_to_macro.py
+++ Opsin_Structure/msm/12macros/micro_to_macro.py
@@ -6,12 +6,14 @@ map15 = np.loadtxt('map12_pccaplus.dat')
 
 def mapp(micro_assigns,macro_map):
     n_macros = int(np.max(macro_map)) + 1
-    macro_assigns = np.zeros(micro_assigns.shape)
+    macro_assigns = np.ones(micro_assigns.shape)  * -100
     for i in range(n_macros):
 	ind_map = np.where(macro_map == i)[0]
 	for m in ind_map:
 		ind = (micro_assigns == m)
 		macro_assigns[ind] = i
+    if [-100] in macro_assigns:
+	print "not all micro assignments are converted"
     return macro_assigns 
 
 for i in range(64):
