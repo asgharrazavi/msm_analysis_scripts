31e1b5afcc08186034a2afb0d09ed9c532013ef6
Asghar Razavi <asghar.razavi@gmail.com>
2018-06-26 09:10:16 -0400

    implied_times2.py

--- hDAT_Na_SREP/msm/implied_times2.py
+++ hDAT_Na_SREP/msm/implied_times2.py
@@ -10,11 +10,10 @@ sequences = np.loadtxt('assigns_all_skip20.txt',dtype=int)
 lag_times = [5,10,15,20,30,50,100,200,300]
 n_timescales = 10
 
-def plot(data,title,outname):
+def plot(data,outname):
     plt.figure()
     for i in range(n_timescales):
    	plt.plot(lag_times, data[:, i], 'o-')
-    plt.title(title)
     plt.semilogy()
     plt.xticks([5,15,30,50,100],np.array([5,15,30,50,100])*20*80/1000,fontsize=18)
     plt.yticks(fontsize=18)
@@ -25,5 +24,5 @@ def plot(data,title,outname):
 
 msm_timescales_d = implied_timescales(sequences, lag_times, n_timescales=n_timescales, n_jobs=1, \
 				      msm=MarkovStateModel(verbose=True,  reversible_type='transpose',ergodic_cutoff=0),verbose=1)
-plot(msm_timescales_d,'Discrete-time MSM Relaxation Timescales','imp_test.png')
+plot(msm_timescales_d,'img_implied_times.png')
 
