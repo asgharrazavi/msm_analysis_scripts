dd3da825dad88588ffc52e73b3cab4d8808d854e
Asghar Razavi <asghar.razavi@gmail.com>
2018-06-26 09:09:36 -0400

    all

--- hDAT_Na_SREP/msm/implied_times2.py
+++ hDAT_Na_SREP/msm/implied_times2.py
@@ -7,7 +7,6 @@ mpl.rcParams['axes.linewidth'] = 1 #set the value globally
 
 sequences = np.loadtxt('assigns_all_skip20.txt',dtype=int)
 
-lag_times = [5,10,15,20,30,50,100]
 lag_times = [5,10,15,20,30,50,100,200,300]
 n_timescales = 10
 
@@ -24,14 +23,7 @@ def plot(data,title,outname):
     plt.savefig(outname)
     plt.close()
 
-msm_timescales_d = implied_timescales(sequences, lag_times, n_timescales=n_timescales, n_jobs=1,msm=MarkovStateModel(verbose=True,  reversible_type='transpose',ergodic_cutoff=0),verbose=1)
+msm_timescales_d = implied_timescales(sequences, lag_times, n_timescales=n_timescales, n_jobs=1, \
+				      msm=MarkovStateModel(verbose=True,  reversible_type='transpose',ergodic_cutoff=0),verbose=1)
 plot(msm_timescales_d,'Discrete-time MSM Relaxation Timescales','imp_test.png')
-quit()
-
-#msm_timescales_d_mle = implied_timescales(sequences, lag_times, n_timescales=n_timescales, n_jobs=1,msm=MarkovStateModel(verbose=True,  reversible_type='mle',ergodic_cutoff=1),verbose=1)
-msm_timescales_d_mle = implied_timescales(sequences, lag_times, n_timescales=n_timescales, n_jobs=1,msm=MarkovStateModel(verbose=True),verbose=1)
-plot(msm_timescales_d_mle,'Discrete-time MSM Relaxation Timescales MLE','imp_times_mle.png')
-
-msm_timescales_c = implied_timescales(sequences, lag_times, n_timescales=n_timescales, n_jobs=1, msm=ContinuousTimeMSM(verbose=True),verbose=1)
-plot(msm_timescales_c,'Continuous-time MSM Relaxation Timescales MLE','imp_times_c.png')
 
