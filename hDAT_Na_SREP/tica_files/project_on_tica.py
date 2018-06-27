c2299645c6ab74314553e0149ad9bfe5d7578f5c
Asghar Razavi <asghar.razavi@gmail.com>
2018-06-27 07:13:31 -0400

    project_on_tica.py

--- hDAT_Na_SREP/tica_files/project_on_tica.py
+++ hDAT_Na_SREP/tica_files/project_on_tica.py
@@ -9,7 +9,6 @@ from matplotlib import rcParams
 rcParams.update({'figure.autolayout': True})
 rcParams['axes.linewidth'] = 3
 
-
 def project(name):
     dataset, ev0, ev1 = [], [], []
     for i in range(50):
