9dd5f1c648c5317bb1b74a74e3d75c01879ce932
Asghar Razavi <asghar.razavi@gmail.com>
2018-06-28 12:49:58 -0400

    micro_to_macro.py

--- hDAT_Na_SREP/msm/15macros/micro_to_macro.py
+++ hDAT_Na_SREP/msm/15macros/micro_to_macro.py
@@ -2,7 +2,7 @@ import os,sys
 import mdtraj.io as io
 import numpy as np
 
-map15 = np.loadtxt('map12_pccaplus.dat')
+map15 = np.loadtxt('map15_pccaplus.dat')
 
 def mapp(micro_assigns,macro_map):
     n_macros = int(np.max(macro_map)) + 1
@@ -16,8 +16,7 @@ def mapp(micro_assigns,macro_map):
 	print "not all micro assignments are converted"
     return macro_assigns 
 
-for i in range(64):
-    micro_assigns = np.loadtxt('../../assigns/assigns_%d.txt' %(i),dtype=int)
-    macro_assigns = mapp(micro_assigns,map15)
-    print "i, micro_assigns.shape:", i, micro_assigns.shape
-    np.savetxt('macro12_assigns_%d.txt' %i, macro_assigns, fmt='%d')
+micro_assigns = np.loadtxt('assigns_all_skip20.txt',dtype=int)
+macro_assigns = mapp(micro_assigns,map15)
+print "i, micro_assigns.shape:", i, micro_assigns.shape
+np.savetxt('macro15_assigns.txt', macro_assigns, fmt='%d')
