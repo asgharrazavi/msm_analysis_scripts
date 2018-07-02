e8b3c43fb755e84d47b868979b0de7265e5220a6
Asghar Razavi <asghar.razavi@gmail.com>
2018-07-02 14:40:48 -0400

    tpt.py

--- hDAT_Na_SREP/msm/15macros/tpt.py
+++ hDAT_Na_SREP/msm/15macros/tpt.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 from matplotlib import rcParams
 rcParams.update({'figure.autolayout': True})
 
-assigns = np.loadtxt('macro_assigns_map15_pccaplus.txt' ,dtype=int)
+assigns = np.loadtxt('macro15_assigns.txt' ,dtype=int)
 msm = MarkovStateModel(lag_time=30, n_timescales=20, reversible_type='transpose', ergodic_cutoff='off', prior_counts=0, sliding_window=True, verbose=True)
 msm.fit(assigns)
 
@@ -13,12 +13,9 @@ def do_tpt(ev_id):
     plt.figure(figsize=(15,10))
     sources = [14]
     sinks = [5,7,2,3,1,0]
-#    sources = np.where(msm.left_eigenvectors_[:,ev_id] < -0.05)
-#    sinks = np.where(msm.left_eigenvectors_[:,ev_id] > 0.05)
     net_flux = tpt.net_fluxes(sources, sinks, msm, for_committors=None)
     pfold = tpt.committors(sources, sinks, msm)
     paths = tpt.paths(sources, sinks, net_flux, remove_path='subtract', flux_cutoff=0.9999999999)
-#    mfpts = tpt.mfpts(msm, sinks, lag_time=20.0)
     sort = np.argsort(pfold)
     total_line_width = np.sum(paths[1][0:5])
     for j in range(5):
@@ -29,11 +26,6 @@ def do_tpt(ev_id):
 	    x.extend(np.where(np.arange(100)[sort] == paths[0][j][k])[0])
 	plt.plot(x,pfold[paths[0][j]],linewidth=np.log(paths[1][j]/float(total_line_width)))
     plt.legend(['%1.8f' %i for i in paths[1][0:5]],fontsize=18,loc='upper left')
-    for i in range(len(pfold)):
-       	plt.plot(i,pfold[sort[i]],'o')
-       	plt.text(i,pfold[sort[i]],sort[i])
-    plt.savefig('pfold_ev%d_0.01.png' %ev_id)
-
 
 for i in [1,8]:
     print "working on eigenvector:", i
