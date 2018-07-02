967f3e32c761d920d9763994863084948efe35db
Asghar Razavi <asghar.razavi@gmail.com>
2018-07-02 14:45:48 -0400

    tpt.py

--- hDAT_Na_SREP/msm/15macros/tpt.py
+++ hDAT_Na_SREP/msm/15macros/tpt.py
@@ -4,6 +4,7 @@ from msmbuilder.msm import ContinuousTimeMSM, MarkovStateModel
 import matplotlib.pyplot as plt
 from matplotlib import rcParams
 rcParams.update({'figure.autolayout': True})
+from tabulate import tabulate
 
 assigns = np.loadtxt('macro15_assigns.txt' ,dtype=int)
 msm = MarkovStateModel(lag_time=30, n_timescales=20, reversible_type='transpose', ergodic_cutoff='off', prior_counts=0, sliding_window=True, verbose=True)
@@ -20,7 +21,7 @@ def do_tpt():
     paths = tpt.paths(sources, sinks, net_flux, remove_path='subtract', flux_cutoff=0.9999999999)
     sort = np.argsort(pfold)
     total_line_width = np.sum(paths[1][0:5])
-    print tabulate(paths,header=('Path','Flux'))
+    print tabulate([paths[0],paths[1]],headers=('Path','Flux'))
     for j in range(10):
 	print "path:", paths[0][j]
 	print "flux:", paths[1][j]
