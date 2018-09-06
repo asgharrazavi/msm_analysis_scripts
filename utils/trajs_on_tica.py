666836caed1f02e630156ba40826c2debd1356ad
Asghar Razavi <asghar.razavi@gmail.com>
2018-09-06 13:24:28 -0400

    trajs_on_tica.py

---
+++ utils/trajs_on_tica.py
@@ -0,0 +1,33 @@
+import numpy as np
+import mdtraj.io as io
+import mdtraj as md
+import matplotlib.pyplot as plt
+from matplotlib.colors import LogNorm
+from matplotlib import rcParams
+rcParams.update({'figure.autolayout': True})
+rcParams['axes.linewidth'] = 3
+rcParams.update({'font.size': 20})
+from tqdm import tqdm
+import matplotlib.cm as cm
+
+ev0 = io.loadh('ev0.h5')['arr_0']
+ev1 = io.loadh('ev1.h5')['arr_0']
+#ev2 = io.loadh('ev2.h5')['arr_0']
+#plt.hist2d(ev0,ev1,bins=500,norm=LogNorm(),alpha=0.6)
+
+plt.figure(figsize=(15,5))
+ii = 1
+for i in tqdm(['A','B','C']):
+    plt.subplot(1,3,ii)
+    d = io.loadh('on_tica_l20_%s.h5'%i)['arr_0'][:,0:2]
+    plt.hist2d(ev0,ev1,bins=300,norm=LogNorm(),alpha=0.6)
+    for j in tqdm(range(d.shape[0]/100)):
+        color = cm.jet(float(j)/(d.shape[0]/100),1)
+        plt.plot(d[:,0][::100][j],d[:,1][::100][j],'o',color=color,markersize=8,alpha=0.8)
+    plt.title(i)
+    plt.xticks([])   
+    plt.yticks([])   
+    ii += 1
+plt.savefig('trajs_on_tica.png')
+plt.show()
+
