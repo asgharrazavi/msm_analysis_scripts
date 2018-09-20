d20eed08b90efcdba778f90829d4c8f455f551bc
Asghar Razavi <asghar.razavi@gmail.com>
2018-09-20 12:12:50 -0400

    cal_cc_dists.py

--- utils/cal_cc_dists.py
+++ utils/cal_cc_dists.py
@@ -67,7 +67,7 @@ glu = ref.top.select('protein and resname GLU and name CD')
 sap4 = ref.top.select('segname SAP4 and name P')
 sap5 = ref.top.select('segname SAP5 and name P')
 
-# load trajectories, calculates charged residue distances and save as hdf5 file
+# load trajectories, calculates charged residue distances and save as hdf5 format
 for i in range(50):
     print "working on trajectory:\t", i
     traj_path = '/Users/asr2031/Desktop/transfer/dDAT_WT_ensemble_stampede/1to781_skip20/traj%d_whole_1to781_skip20.xtc'  %i
