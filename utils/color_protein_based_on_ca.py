e3ffa3305265c037593fe46cdc3e74626e9f08ac
Asghar Razavi <asghar.razavi@gmail.com>
2018-10-18 12:47:53 -0400

    color_protein_based_on_ca.py

--- utils/color_protein_based_on_ca.py
+++ utils/color_protein_based_on_ca.py
@@ -1,11 +1,12 @@
 import numpy as np
 import mdtraj as md
 
+# load inputs
 ca = np.loadtxt('chol_prot_popc.txt')
-
 ref = md.load('protein_no_chol_beta_occ.pdb')
 print "number of atoms:", ref.top.n_atoms
 
+# make a list based on ca values
 s = []
 for i in range(ref.n_residues):
     r = ref.top.residue(i)
@@ -16,4 +17,3 @@ print "number of data points:", len(s)
 # len(s) should match ref.top.n_atoms
 
 np.savetxt('tmp.txt',s,fmt='%1.2f')
-
