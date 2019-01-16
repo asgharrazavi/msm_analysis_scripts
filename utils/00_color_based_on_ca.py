50d58fedab3e0cdcf86aee21f9e27d0757863cb2
Asghar Razavi <asghar.razavi@gmail.com>
2019-01-16 12:24:22 -0500

    00_color_based_on_ca.py

--- utils/00_color_based_on_ca.py
+++ utils/00_color_based_on_ca.py
@@ -6,13 +6,13 @@ ca = np.loadtxt('chol_prot_popc2.txt')
 ref = md.load('protein.pdb')
 print "nubmer of atoms:",ref.top.n_atoms
 
-# saving a text file for color values that can be used in visualization
+# save a text file for color values ifor each residue that can be used in VMD for visualization
 s = []
 for i in range(ref.n_residues):
     r = ref.top.residue(i)
     for j in range(r.n_atoms):
 	s.append(ca[i])
 
-print "n data:", len(s)
+print "number of data points:", len(s)
 np.savetxt('tmp.txt',s,fmt='%1.2f')
 
