57b47edd8a291327fd5b8a0f3f7dc4ba294b5f01
Asghar Razavi <asghar.razavi@gmail.com>
2019-01-16 12:22:51 -0500

    00_color_based_on_ca.py

---
+++ utils/00_color_based_on_ca.py
@@ -0,0 +1,21 @@
+import numpy as np
+import mdtraj as md
+
+ca1 = np.loadtxt('chol_prot_popc2.txt')
+ca2 = np.loadtxt('no_chol_prot_popc2.txt')
+
+ca = abs(ca2 - ca1) 
+#ca = ca * ca2
+
+ref = md.load('protein_no_chol_beta_occ.pdb')
+print "n atoms:",ref.top.n_atoms
+
+s = []
+for i in range(ref.n_residues):
+    r = ref.top.residue(i)
+    for j in range(r.n_atoms):
+	s.append(ca[i])
+
+print "n data:", len(s)
+np.savetxt('tmp.txt',s,fmt='%1.2f')
+
