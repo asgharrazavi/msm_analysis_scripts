cae58c795b2341987c38bef25d9a636356248e7d
Asghar Razavi <asghar.razavi@gmail.com>
2019-01-31 08:52:55 -0500

    util_functions.py

--- utils/util_functions.py
+++ utils/util_functions.py
@@ -3,7 +3,9 @@
 
 def cal_on_off_time(data):
     """
-
+    inputs:
+    ------
+    data: 1D numpy array with only 0, 1 entries
     """
 
         d = np.diff(np.pad(c, pad_width=1, mode='constant'))
