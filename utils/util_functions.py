c3c89dedc3bfe18f3b6426d0d1ed51fc990d1891
Asghar Razavi <asghar.razavi@gmail.com>
2019-01-31 08:51:57 -0500

    util_functions.py

--- utils/util_functions.py
+++ utils/util_functions.py
@@ -1,4 +1,10 @@
-# some
+# some utility functions
+
+
+def cal_on_off_time(data):
+    """
+
+    """
 
         d = np.diff(np.pad(c, pad_width=1, mode='constant'))
         r = np.flatnonzero(d == -1) - np.flatnonzero(d == 1)
