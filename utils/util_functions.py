e8e7285da71ec3565a9848a5bd98d3f5d2043582
Asghar Razavi <asghar.razavi@gmail.com>
2019-01-31 08:53:51 -0500

    util_functions.py

--- utils/util_functions.py
+++ utils/util_functions.py
@@ -7,6 +7,8 @@ def cal_on_off_time(data):
     ------
     data: 1D numpy array with only 0, 1 entries
     """
+    # source: https://stackoverflow.com/questions/54446907/how-to-calculate-numbers-of-uninterrupted-repeats-in-an-array-in-python?noredirect=1#comment95703294_54446907 
+    d = np.diff(np.pad(data, pad_width=1, mode='constant'))
+    r = np.flatnonzero(d == -1) - np.flatnonzero(d == 1)
 
-        d = np.diff(np.pad(c, pad_width=1, mode='constant'))
-        r = np.flatnonzero(d == -1) - np.flatnonzero(d == 1)
+
